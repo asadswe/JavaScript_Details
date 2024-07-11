897d70229e6113ad07bd0f4c3a68596b2c49fa4e
MD. ASADUZZAMAN <asaduzzaman.swe@gmail.com>
2024-07-11 21:13:49 +0600

    Add files via upload

---
+++ Part2_addEventListener/Part1/app.js
@@ -0,0 +1,5 @@
+//btn er text o change kora jayi, cick diye
+
+const button = document.getElementById("myButton").addEventListener("click", function(){
+    document.getElementById("myButton").innerHTML = "Ki obsta";
+})
