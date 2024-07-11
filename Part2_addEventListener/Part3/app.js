aeecc3d0c0e6ad3fb0797634a963fbf5cd33479e
MD. ASADUZZAMAN <asaduzzaman.swe@gmail.com>
2024-07-11 22:09:02 +0600

    Add files via upload

---
+++ Part2_addEventListener/Part3/app.js
@@ -0,0 +1,4 @@
+const button = document.getElementById("myButton").addEventListener("click", function(){
+    const output = document.getElementById('output');
+    output.textContent = 'Asaduzzaman';
+})
